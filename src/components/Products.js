ff8b0c7df5f4b09e7852c78559c55de555a524d4
Vishal <50748766+vishal-koundal@users.noreply.github.com>
2020-03-18 00:36:41 -0700

    minor fixes

--- src/components/Products.js
+++ src/components/Products.js
@@ -71,7 +71,7 @@ const Products = () => (
   <>
     {StoreProducts.map(item => (
       <div key={item.id} className="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
-        <a href="/">
+        <a href="/product">
           <img
             className="hover:grow hover:shadow-lg"
             src={item.img}
